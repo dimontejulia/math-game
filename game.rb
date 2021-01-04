8731933d8cae1de0c342376b06703a247aae023c
dimontejulia <dimontejulia@gmail.com>
2021-01-04 14:45:47 -0500

    added random number coin toss to see who goes first

--- game.rb
+++ game.rb
@@ -4,23 +4,36 @@ require "./question.rb"
 class Game
   attr_accessor :lives
 
-  # player 1 starts game
-  player1 = Player.new(true)
-  player2 = Player.new(false)
-
   turnNum = 0
 
   puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   puts "Hello and welcome to the math game!"
   puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n "
 
+  # random number to see who goes first
+  coinToss = rand(0..1)
+  puts "Tossing the coin to see who goes first..."
+  sleep 2
+  if (coinToss == 0)
+    puts "\nPlayer 1 won the coin toss! They will go first.\n "
+    player1 = Player.new(true)
+    player2 = Player.new(false)
+  else
+    puts "\nPlayer 1 won the coin toss! They will go first.\n "
+    player1 = Player.new(false)
+    player2 = Player.new(true)
+  end
+  sleep 2
+
   while (player1.alive? && player2.alive?) do
+    #turn counter
     turnNum +=1
     puts "----------TURN #{turnNum}----------"
+
+    #player 1 turn
     if (player1.currentTurn?)
       question = Question.new()
       puts "P1: #{question.math_question}"
-      user_answer = gets.chomp.to_i
       if (question.correctAnswer == user_answer)
         puts 'YES! You are correct.'
         puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
@@ -33,28 +46,29 @@ class Game
         player1.turn = false
         player2.turn = true
       end
-    
-
-  else (player2.currentTurn?)
-      question = Question.new()
-      puts "P2: #{question.math_question}"
-      puts question.correctAnswer
-      user_answer = gets.chomp.to_i
-      if (question.correctAnswer == user_answer)
-        puts 'YES! You are correct.'
-        puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
-        player1.turn = true
-        player2.turn = false
-      else
-        puts "Seriously? NO!"
-        player2.lives -= 1
-        puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
-        player1.turn = true
-        player2.turn = false
+      
+    #player 2 turn
+    else (player2.currentTurn?)
+        question = Question.new()
+        puts "P2: #{question.math_question}"
+        puts question.correctAnswer
+        user_answer = gets.chomp.to_i
+        if (question.correctAnswer == user_answer)
+          puts 'YES! You are correct.'
+          puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+          player1.turn = true
+          player2.turn = false
+        else
+          puts "Seriously? NO!"
+          player2.lives -= 1
+          puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+          player1.turn = true
+          player2.turn = false
+        end
       end
-    end
   end
   
+  #game over - announce winner and loser
   puts "~~~~~~~~~~~~~~~~~~GAME OVER~~~~~~~~~~~~~~~~~~~~"
   if (player1.lives == 0)
     puts "PLAYER 2 WINS!!!!!"
